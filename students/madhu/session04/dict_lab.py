5d00d0c2df825be8fbebaf9e00ebad38b198f3a9
Madhumita Acharya <madhu84.bi@gmail.com>
2016-10-21 13:26:04 -0700

    session 04 exercises

--- students/madhu/session04/dict_lab.py
+++ students/madhu/session04/dict_lab.py
@@ -16,15 +16,38 @@ def dict_key_values(dict):
     all_keys = dict.keys()
     print all_keys
 
+def dict_values(dict):
+    all_values = dict.values()
+    return all_values
+
 if __name__ == '__main__':
+    print("\n") #inserting a newline character for better readability#
+
     print("The dictionary to start with:")
     display_dict(d)
+
+    print("\n") #inserting a newline character for readability#
+
     print("The dictionary after deleting the entry for cake:")
     b = delete_dict(d,'cake')
     display_dict(b)
+
+    print("\n") #inserting a newline character for readability#
+
     print("The dictionary after adding a new fruit called Mango:")
     c = add_dict(b,'fruits','Mango')
     display_dict(c)
-    print("The key values in a dictionary are:")
+
+    print("\n") #inserting a newline character for readability#
+
+    print("The keys in the dictionary are:")
     dict_key_values(c)
+
+    print("\n") #inserting a newline character for readability#
+
+    print("The values in the dictionary are:")
+    d = dict_values(c)
+    print(d)
+
+    print("\n") #inserting a newline character for readability#
 
